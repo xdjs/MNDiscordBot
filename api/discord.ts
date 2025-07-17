f58c5d5b62b2dab0a15fffbe494304d5bc26897f
Cyber-Ivern <avery.kwok05@yahoo.com>
2025-07-17 11:53:03 -0700

    changed some imports to compile time instead of runtime

--- api/discord.ts
+++ api/discord.ts
@@ -1,4 +1,4 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { verifyKey, InteractionType, InteractionResponseType } from 'discord-interactions';
 import 'dotenv/config';
 
