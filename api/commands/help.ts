73c8e417c86aec626ef77fca66c1b4a99b03ec34
Cyber-Ivern <avery.kwok05@yahoo.com>
2025-07-22 16:36:51 -0700

    dynamically changing footer prompt

--- api/commands/help.ts
+++ api/commands/help.ts
@@ -3,7 +3,9 @@ import { InteractionResponseType } from 'discord-interactions';
 const BOT_TOKEN = process.env.DISCORD_BOT_TOKEN!;
 
 export async function help(userId: string) {
-  const helpText = `Here are the available commands:
+  const helpText = `For the best experience of this bot, please turn off message notification sounds in settings->notifications->message notifications->turn off.
+  \n\n
+  \nHere are the available commands:
   \n/help - Show this help message
   \n/hi - Say hi!
   \n/connect - Link your Spotify account
@@ -12,6 +14,7 @@ export async function help(userId: string) {
   \n/profile - Gets a custom profile card displayed in the channel.
   \n/image - Generate a personalized picture of you listening to your top 10 Spotify tracks.
   \n/setimage - Use your generated image as profile background.
+  \n/chat - Start a chat with the bot.(must have a text channel labeled "bot-chat")
   \n/listen - Give you fun facts about the artist you are listening to.
   \nTo make this work please connect Spotify to your Discord (Settings → Connections) and enable the \"Display current activity\" option.\nStart listening to a song and then type /listen to get fun facts about the artist.
   \n/listen user<username> - Give you fun facts about the artist that user is listening to. (has to have Spotify status enabled).
